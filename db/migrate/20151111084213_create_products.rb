806923605ec3d860863eab797d18e8379f360906
Tracy Mu Sung <tracy.musung@moomumedia.com>
2015-11-14 14:53:45 +1100

    DB not need images, upload instead

--- db/migrate/20151111084213_create_products.rb
+++ db/migrate/20151111084213_create_products.rb
@@ -5,10 +5,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :product_code
       t.text :product_features
       t.text :dimensions
-      t.string :image_url
-      t.string :spec_url
       t.timestamps
-
     end
   end
 end
